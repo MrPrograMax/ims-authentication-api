5a2e1ef283d713d4bdf9409df3658d390027ca21
MrPrograMax <grigorievma9461@gmail.com>
2024-06-28 00:49:25 +0700

    change: user model

--- model/user.go
+++ model/user.go
@@ -2,6 +2,8 @@ package model
 
 type User struct {
 	Id       int64  `json:"-"`
+	FullName string `json:"full_name" binding:"required" db:"full_name"`
+
 	Login    string `json:"login" binding:"required" db:"login"`
 	Password string `json:"password" binding:"required" db:"password"`
 	RoleId   int64  `json:"role_id" binding:"required" db:"role_id"`
